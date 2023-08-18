23f730522f3929449b7933bd43957528fd2011eb
Anto Alex <antoalex710@gmail.com>
2023-08-18 23:08:05 +0530

    updated security configurations

--- src/main/java/com/antoalex07/jmfoods/config/SecurityConfig.java
+++ src/main/java/com/antoalex07/jmfoods/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.antoalex07.jmfoods.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -12,7 +13,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
 
 
 @Configuration
@@ -21,16 +21,19 @@ public class SecurityConfig{
     
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
-        http.authorizeHttpRequests(auth ->
-        auth.requestMatchers("/admin/**").hasRole("ADMIN")
-        .anyRequest().permitAll())
-        .formLogin(withDefaults());
-
-        return http.build();
+        return http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> {
+                auth.requestMatchers("/").permitAll();
+                auth.requestMatchers("/client").permitAll();
+                auth.requestMatchers("/admin").hasRole("ADMIN");
+            })
+            .httpBasic(Customizer.withDefaults())
+            .build();
     }
 
     @Bean
-    UserDetails userDetails(){
+    InMemoryUserDetailsManager userDetails(){
         UserDetails admin = User.builder()
             .username("admin")
             .passwordEncoder(passwordEncoder()::encode)
@@ -38,9 +41,9 @@ public class SecurityConfig{
             .roles("ADMIN")
             .build();
         
-        return (UserDetails) new InMemoryUserDetailsManager(admin); 
+        return new InMemoryUserDetailsManager(admin); 
     }
-
+    
     @Bean
     PasswordEncoder passwordEncoder(){
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
